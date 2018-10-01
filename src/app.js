709d283aa6c17f4cde2ccbe18c15c3d8121dfa4c
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-10-01 20:19:52 +0900

    Promisify

--- src/app.js
+++ src/app.js
@@ -2,40 +2,50 @@ import TilecloudControl from '@tilecloud/mbgl-tilecloud-control'
 import mapboxgl from 'mapbox-gl'
 
 const onceRendered = {}
+const $css = Symbol('css')
 
 export const render = elementId => {
-  const onScrollEventHandler = () => {
-    console.log('scrolling')
-    if (!onceRendered[elementId] && isInVerticalView(elementId)) {
-      onceRendered[elementId] = true
-      console.log('start map rendering')
+  return new Promise((resolve, reject) => {
+    const onScrollEventHandler = () => {
+      console.log('scrolling')
+      if (!onceRendered[elementId] && isInVerticalView(elementId)) {
+        onceRendered[elementId] = true
+        console.log('start map rendering')
 
-      fetch('./mapbox-gl.css')
-        .then(res => res.text())
-        .then(cssText => {
-          const head = document.getElementsByTagName('head')[0]
-          const style = document.createElement('style')
-          style.innerText = cssText.replace(/\n/g, '')
-          head.appendChild(style)
-        })
+        !onceRendered[$css] &&
+          fetch('./mapbox-gl.css')
+            .then(res => res.text())
+            .then(cssText => {
+              const head = document.getElementsByTagName('head')[0]
+              const style = document.createElement('style')
+              style.innerText = cssText.replace(/\n/g, '')
+              head.appendChild(style)
+            })
+        onceRendered[$css] = true
 
-      const tilecloudMap = new mapboxgl.Map({
-        container: elementId,
-        style: 'https://tilecloud.github.io/tiny-tileserver/style.json',
-        attributionControl: true,
-        hash: true,
-        localIdeographFontFamily: 'sans-serif',
-        interactive: true,
-      })
+        let map
+        try {
+          map = new mapboxgl.Map({
+            container: elementId,
+            style: 'https://tilecloud.github.io/tiny-tileserver/style.json',
+            attributionControl: true,
+            hash: true,
+            localIdeographFontFamily: 'sans-serif',
+            interactive: true,
+          })
 
-      tilecloudMap.addControl(new mapboxgl.NavigationControl())
-      tilecloudMap.addControl(new mapboxgl.GeolocateControl())
-      tilecloudMap.addControl(new TilecloudControl())
-      return tilecloudMap
+          map.addControl(new mapboxgl.NavigationControl())
+          map.addControl(new mapboxgl.GeolocateControl())
+          map.addControl(new TilecloudControl())
+        } catch (e) {
+          reject(e)
+        }
+
+        resolve(map)
+      }
     }
-  }
-
-  window.addEventListener('scroll', onScrollEventHandler, false)
+    window.addEventListener('scroll', onScrollEventHandler, false)
+  })
 }
 
 const isInVerticalView = elementId => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // import 'babel-polyfill'
 import { render } from './app'
 
-render('map-1')
-render('map-2')
+render('map-1').then(() => alert('started rendering map-1'))
+render('map-2').then(() => alert('started rendering map-2'))
